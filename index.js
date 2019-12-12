fe8ea91441996e72c43c06d98040d1ee3b5ef60a
Rand Al'Thor <alexanderhogle@gmail.com>
2019-12-11 20:14:01 -0600

    generate html function

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ inquirer
     name: "username"
   })
   .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    const queryUrl = `https://api.github.com/users/${username}`;
 
     axios.get(queryUrl).then(function(res) {
       const repoNames = res.data.map(function(repo) {
@@ -26,3 +26,43 @@ inquirer
       });
     });
   });
+
+  function generateHTML(answers) {
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${res.name}</h1>
+      <p class="lead">I am from ${res.location}.</p>
+      <ul class="list-group">
+      <li class="list-group-item">Public Repositories: ${res.public_repos}</li>
+        <li class="list-group-item">Followers ${res.followers}</li>
+        <li class="list-group-item">GitHub Stars: ${res.public_gists}</li>
+        <li class="list-group-item">Following: ${res.following}</li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`;
+  }
+  
+  promptUser()
+    .then(function(res) {
+      const html = generateHTML(res);
+  
+      return writeFileAsync("index.html", html);
+    })
+    .then(function() {
+      console.log("Successfully wrote to index.html");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
