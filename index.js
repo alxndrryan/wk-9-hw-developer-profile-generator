9ea02e86a40bf9f9160227434063c154bea6b3f8
Rand al'Thor <alexanderhogle@gmail.com>
2020-02-11 00:13:46 -0600

    generates html and creates pdf file

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
-const fs = require("fs");
-const axios = require("axios");
+const genHTML = require("./generateHTML.js");
 const inquirer = require("inquirer");
+const axios = require("axios");
+const pdf = require('html-pdf');
+const fs = require('fs');
+var options = { format: 'Letter' };
 
-inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
-    name: "username"
-  })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+const questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your Github username:"
+    },
+    {
+        type: "list",
+        message: "Choose page color: ",
+        name: "color",
+        choices: [
+            "green",
+            "blue",
+            "red"
+        ]
+    }
+];
 
-    axios.get(queryUrl).then(function(res) {
-      const repoNames = res.data.map(function(repo) {
-        return repo.name;
-      });
+function writeToFile(fileName, data) {
+    fs.writeFile("profile.html", genHTML.generateHTML(data), function (err) {
+        if (err) throw err;
+        console.log("HTML page generated!");
+        let html = fs.readFileSync('./profile.html', 'utf8');
+        pdf.create(html, options).toFile(fileName, function (err) {
+            if (err) return console.log(err);
+            console.log(`PDF document generated as ${fileName}`);
+        });
+    })
+}
 
-      const repoNamesStr = repoNames.join("\n");
+function init() {
+    inquirer.prompt(questions).then(function (userInfo) {
+        // Assign stars
+        let queryURL = `https://api.github.com/users/${userInfo.username}/starred?per_page=10000`;
+        axios.get(queryURL).then(response => {
+            let starred = { starred: Object.keys(response.data).length };
+            Object.assign(userInfo, starred);
 
-      fs.writeFile("repos.txt", repoNamesStr, function(err) {
-        if (err) {
-          throw err;
-        }
-
-        console.log(`Saved ${repoNames.length} repos`);
-      });
+            // Assign remaining info
+            let queryURL = `https://api.github.com/users/${userInfo.username}`;
+            axios.get(queryURL).then(response => {
+                Object.assign(userInfo, response.data);
+                writeToFile(`${userInfo.username}Profile.pdf`, userInfo);
+            })
+        }, e => {
+            console.log(`The file could not be generated. Reason: user does not exist.`);
+        })
     });
-  });
+}
+
+init();
